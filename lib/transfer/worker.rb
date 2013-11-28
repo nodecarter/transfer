28d9fe9c704f9c0ed78d0ba8353d8f7e172c703f
Danil Tashkinov <danil.tashkinov@gmail.com>
2013-11-28 16:55:26 +0400

    transfer tables without primary key

--- lib/transfer/worker.rb
+++ lib/transfer/worker.rb
@@ -13,15 +13,23 @@ class Transfer::Worker
   end
 
   def source_db
-    @source_db ||= @options[:source_db] || Sequel.connect(config.source)
+    @source_db ||= begin
+      db = @options[:source_db] || Sequel.connect(config.source)
+      #db.logger = logger
+      db
+    end
   end
 
   def target_db
-    @target_db ||= @options[:target_db] || Sequel.connect(config.target)
+    @target_db ||= begin
+      db = @options[:target_db] || Sequel.connect(config.target)
+      #db.logger = logger
+      db
+    end
   end
 
   def transfer_all
-    target_db.transaction do
+    target_db.transaction(rollback: :always) do
       table_names.each do |table_name|
         transfer_table table_name
       end
@@ -29,26 +37,14 @@ class Transfer::Worker
   end
 
   def transfer_table(table_name)
-    # checks before:
-    #   source:
-    #     - table exists
-    #   target:
-    #     - table exists
-    #     - table empty
-    # checks after:
-    #   target:
-    #     - records count the same as in source
-    #     - next autoincrement value
-
     log "transfer table #{table_name}"
-    validator = validator(table_name)
-    validator.validate_before!(table_name)
-    source_model = Sequel::Model(table_name)
-    source_model.db = source_db
-    source_db[table_name].order(source_model.primary_key).paged_each do |source_row|
-      target_db[table_name].insert(source_row)
+    if target_db.in_transaction?
+      actual_transfer_table(table_name)
+    else
+      target_db.transaction(rollback: :always) do
+        actual_transfer_table(table_name)
+      end
     end
-    validator.validate_after!(table_name)
   end
 
   def validator(table_name)
@@ -64,6 +60,41 @@ class Transfer::Worker
   end
 
   private
+
+  def actual_transfer_table(table_name)
+    validator = validator(table_name)
+    validator.validate_before!(table_name)
+
+    buf_size = 1000
+    target_buf = []
+    columns = nil
+    source_fetch(table_name) do |source_row|
+      columns ||= source_row.keys
+      target_buf << source_row.values
+      if target_buf.length > buf_size
+        target_db[table_name].import(columns, target_buf)
+        target_buf.clear
+      end
+    end
+    if target_buf.length > 0
+      target_db[table_name].import(columns, target_buf)
+    end
+    validator.validate_after!(table_name)
+  end
+
+  def source_fetch(table_name)
+    source_model = Sequel::Model(table_name)
+    source_model.db = source_db
+    if source_model.primary_key.is_a? Symbol
+      source_db[table_name].order(source_model.primary_key).paged_each do |source_row|
+        yield source_row
+      end
+    else
+      source_db[table_name].all do |source_row|
+        yield source_row
+      end
+    end
+  end
 
   def logger
     @logger ||= @options[:logger] || create_default_logger
--- rakelib/transfer.rake
+++ rakelib/transfer.rake
@@ -2,7 +2,12 @@ desc 'transfer all tables'
 task :transfer => [:environment] do
   config = Transfer::Config.new(File.expand_path('config/transfer.yml', APP_ROOT))
   worker = Transfer::Worker.new config
-  worker.transfer_all
+  table_name = ENV['TABLE_NAME']
+  if table_name.blank?
+    worker.transfer_all
+  else
+    worker.transfer_table(table_name.to_sym)
+  end
 end
 
 task :environment do
